a47bfba6fd06194eedffd58d37037ae8b34d8ff4
Shruti Sharma <98698727+shruti2522@users.noreply.github.com>
2026-03-12 05:14:42 +0530

    feat: add thin-vec Trace feature for boa_engine support (#46)

--- oscars/src/collectors/mark_sweep/gc_collections.rs
+++ oscars/src/collectors/mark_sweep/gc_collections.rs
@@ -202,7 +202,7 @@ mod tests {
 
     #[test]
     fn gc_alloc_vec_basic() {
-        let collector = &MarkSweepGarbageCollector::default();
+        let collector = &mut MarkSweepGarbageCollector::default();
         let vec = GcAllocVec::new_in(collector);
         let gc_vec = Gc::new_in(GcRefCell::new(vec), collector);
 
@@ -214,6 +214,9 @@ mod tests {
         assert_eq!(gc_vec.borrow()[0], 1);
         assert_eq!(gc_vec.borrow()[1], 2);
         assert_eq!(gc_vec.borrow()[2], 3);
+
+        drop(gc_vec);
+        collector.collect();
     }
 
     #[test]
@@ -237,11 +240,14 @@ mod tests {
 
     #[test]
     fn gc_alloc_box_basic() {
-        let collector = &MarkSweepGarbageCollector::default();
+        let collector = &mut MarkSweepGarbageCollector::default();
         let boxed = GcAllocBox::new_in(42u64, collector);
         let gc_box = Gc::new_in(GcRefCell::new(boxed), collector);
 
         assert_eq!(**gc_box.borrow(), 42);
+
+        drop(gc_box);
+        collector.collect();
     }
 
     #[test]
@@ -255,11 +261,14 @@ mod tests {
 
         assert_eq!(gc_box.borrow().len(), 5);
         assert_eq!(gc_box.borrow()[2], 3);
+
+        drop(gc_box);
+        collector.collect();
     }
 
     #[test]
     fn gc_alloc_vec_with_gc_pointers() {
-        let collector = &MarkSweepGarbageCollector::default();
+        let collector = &mut MarkSweepGarbageCollector::default();
         let vec = GcAllocVec::new_in(collector);
         let gc_vec = Gc::new_in(GcRefCell::new(vec), collector);
 
@@ -278,5 +287,8 @@ mod tests {
         assert_eq!(gc_vec.borrow().len(), 2);
         assert_eq!(*gc_vec.borrow()[0].borrow(), 100);
         assert_eq!(*gc_vec.borrow()[1].borrow(), 200);
+
+        drop(gc_vec);
+        collector.collect();
     }
 }
